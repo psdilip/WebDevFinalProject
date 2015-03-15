c9416a48df7e73520ab7c3a38d70ebed9c72629f
psdilip <saidilip26@gmail.com>
2015-03-14 22:22:11 -0600

    Javascript

---
+++ script/slideshow.js
@@ -0,0 +1,10 @@
+<head>
+    <script src = "Homtype = "text/javascript">
+
+    var slideshow = new Array()
+    slideshow[0] = new Picture() 
+    slideshow[0].src = 
+    slideimages[1] = new Image()
+    slideimages[1].src = 
+    slideimages[2] = new Image()
+    slideimages[2].src = 
