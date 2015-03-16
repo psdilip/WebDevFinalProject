072e6c18cea0696ffdf23ebecad1f6a8fbbe277b
Christian Borntrager <christian.borntrager@Gmail.com>
2015-03-16 08:07:55 -0600

    products almost done

--- script/product.js
+++ script/product.js
@@ -1,20 +1,33 @@
-$(document).ready(function () {    
-    $function animate() {
-        drawBox(150,150);
-    }
+$(document).ready(function () {
+    $('#table1').on("click", function () {
+        $('#tabl1').height('500px');
+        $('#table1').width('1000px');
+        $('#table2').height('100px');
+        $('#table2').width('100px');
+        $('#table3').height('100px');
+        $('#table3').width('100px');
+        $('product2').hide();
+        $('product3').hide();
+    });
+    $('#table2').on("click", function () {
+        $('#tabl1').height('100px');
+        $('#table1').width('100px');
+        $('#table2').height('500px');
+        $('#table2').width('1000px');
+        $('#table3').height('100px');
+        $('#table3').width('100px');
+        $('product1').hide();
+        $('product3').toggle();
+    });
+
+    $('#table3').on("click", function () {
+        $('#tabl1').height('100px');
+        $('#table1').width('100px');
+        $('#table2').height('100px');
+        $('#table2').width('100px');
+        $('#table3').height('500px');
+        $('#table3').width('1000px');
+        $('product1').toggle();
+        $('product2').toggle();
+    });
 });
-
-c= document.getElementById("productCanvas");
-ctx=c.getContext("2d");
-
-function drawBox(bx,by) {
-    ctx.beginPath();
-    ctx.rect(bx,by, 60,60);
-    ctx.lineWidth = "4";
-    ctx.strokeStyle = "#40a";
-    ctx.stroke();
-    ctx.closePath;
-}
-
-
-
