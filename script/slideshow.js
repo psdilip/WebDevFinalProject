bd58dc5ab077de153f9fa455976596bd5be28dd4
psdilip <saidilip26@gmail.com>
2015-03-15 18:27:39 -0600

    slideshow

--- script/slideshow.js
+++ script/slideshow.js
@@ -1,10 +1,1 @@
-<head>
-    <script src = "Homtype = "text/javascript">
-
-    var slideshow = new Array()
-    slideshow[0] = new Picture() 
-    slideshow[0].src = 
-    slideimages[1] = new Image()
-    slideimages[1].src = 
-    slideimages[2] = new Image()
-    slideimages[2].src = 
+$("#slideshow > div:gt(0)").hide();
