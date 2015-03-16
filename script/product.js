13dcda04bac0ad53828854e09edcf02426394329
RukaziVrix <ponchitovazquez@gmail.com>
2015-03-15 18:28:35 -0600

    more to canvas

--- script/product.js
+++ script/product.js
@@ -1,4 +1,20 @@
 $(document).ready(function () {    
-    
+    $function animate() {
+        drawBox(150,150);
+    }
 });
+
+c= document.getElementById("productCanvas");
+ctx=c.getContext("2d");
+
+function drawBox(bx,by) {
+    ctx.beginPath();
+    ctx.rect(bx,by, 60,60);
+    ctx.lineWidth = "4";
+    ctx.strokeStyle = "#40a";
+    ctx.stroke();
+    ctx.closePath;
+}
+
+
 
