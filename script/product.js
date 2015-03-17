61df0871e6923eb4866dfa93df85bd37e55b651f
psdilip <saidilip26@gmail.com>
2015-03-16 20:59:56 -0600

    Still in process of Product js

--- script/product.js
+++ script/product.js
@@ -10,36 +10,36 @@ $(document).ready(function () {
     $('#product3').hide();
 
     $('#table1').on("click", function () {
-        $('#tabl1').height('500px');
-        $('#table1').width('1000px');
-        $('#table2').height('100px');
-        $('#table2').width('100px');
-        $('#table3').height('100px');
-        $('#table3').width('100px');
+//        $('#tabl1').height('500px');
+//        $('#table1').width('1000px');
+//        $('#table2').height('100px');
+//        $('#table2').width('100px');
+//        $('#table3').height('100px');
+//        $('#table3').width('100px');
         $('#product1').show();
         $('#product2').hide();
         $('#product3').hide();
         
     });
     $('#table2').on("click", function () {
-     $('#tabl1').height('100px');
-     $('#table1').width('100px');
-      $('#table2').height('500px');
-      $('#table2').width('1000px');
-      $('#table3').height('100px');
-       $('#table3').width('100px');
+//     $('#tabl1').height('100px');
+//     $('#table1').width('100px');
+//      $('#table2').height('500px');
+//      $('#table2').width('1000px');
+//      $('#table3').height('100px');
+//       $('#table3').width('100px');
          $('#product1').hide();
         $('#product2').show();
         $('#product3').hide();
     });
     
     $('#table3').on("click", function () {
-        $('#tabl1').height('100px');
-        $('#table1').width('100px');
-        $('#table2').height('100px');
-        $('#table2').width('100px');
-        $('#table3').height('500px');
-        $('#table3').width('1000px');
+//        $('#tabl1').height('100px');
+//        $('#table1').width('100px');
+//        $('#table2').height('100px');
+//        $('#table2').width('100px');
+//        $('#table3').height('500px');
+//        $('#table3').width('1000px');
          $('#product1').hide();
         $('#product2').hide();
         $('#product3').show();
